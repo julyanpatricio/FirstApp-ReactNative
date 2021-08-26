e21de9bdd9247f33651c31a98a044811c36bb1e1
julyanpatricio <julyan.patricio@gmail.com>
2021-08-26 15:29:38 -0300

    create App with Home & Detail components

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+
+import { PUBLIC_KEY,PRIVATE_KEY,MD5HASH } from '@env';
+
+
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './components/Home'
+import Detail from './components/Detail';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Detail" component={Detail} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
